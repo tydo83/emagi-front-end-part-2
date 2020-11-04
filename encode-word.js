7417d78ddab50747bebf6de7a0eaad7f96bef138
Colin Jaffe <balloonasaurus@gmail.com>
2020-11-04 15:02:25 -0500

    Picks up where we left off.

--- encode-word.js
+++ encode-word.js
@@ -12,30 +12,3 @@ const encodeWord = function(word) {
         }
     }
 }
-
-// mapping version
-const encodeWordAlt = function(word) {
-    const letters = word.split('');
-    const encodedLetters = letters.map(function(letter) {
-        for (const emoji of emojis) {
-            if (emoji.letter === letter) {
-                return emoji.symbol;
-            }
-        }
-    })
-
-    return encodedLetters.join('');
-}
-
-// mapping with no intermediate variables verson.
-const encodeWordAlt2 = function(word) {
-    return word.split('').map(function(letter) {
-        for (const emoji of emojis) {
-            if (emoji.letter === letter) {
-                return emoji.symbol;
-            }
-        }
-    }).join('');
-}
-
-module.exports = encodeWord;
