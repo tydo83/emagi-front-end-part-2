68dae9193c10bf5c6f54c11dcbaee34ba4301e7b
Timothy Do <tydo83@gmail.com>
2020-11-05 02:17:08 -0500

    complete with both ways

--- encode-word.js
+++ encode-word.js
@@ -2,21 +2,35 @@ const emojis = require('./emojis.js');
 
 // string-building version
 // Using A Boolean
+// const encodeWord = function(word) {
+//     let result = '';
+//     let found = false;
+//     for (const char of word) {
+//         for (const emoji of emojis) {
+//             if(emoji.letter === char.toLowerCase()) {
+//                 result += emoji.symbol;
+//                 found = true;
+//             }
+//         }
+//         if(found == false) {
+//             result += char;
+//         }    
+//     }
+//     return result;
+// }
+
+// Using .map
 const encodeWord = function(word) {
-    let result = '';
-    let found = false;
-    for (const char of word) {
-        for (const emoji of emojis) {
-            if(emoji.letter === char.toLowerCase()) {
-                result += emoji.symbol;
-                found = true;
+    let chars = word.toLowerCase().split('');
+        return chars.map(function(char) {
+            for(const emoji of emojis) {
+                if(emoji.letter === char) {
+                    return emoji.symbol;
+                }   
             }
-        }
-        if(found == false) {
-            result += char;
-        }    
-    }
-    return result;
+            return char;
+        }       
+    ).join(' ')
 }
 
 module.exports = encodeWord;
