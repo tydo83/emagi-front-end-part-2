aea4fe9110aa907494f5eabd389a2f1c61065e57
Timothy Do <tydo83@gmail.com>
2020-11-05 01:24:09 -0500

    done with boolean

--- encode-word.js
+++ encode-word.js
@@ -1,14 +1,22 @@
 const emojis = require('./emojis.js');
 
-
 // string-building version
+// Using A Boolean
 const encodeWord = function(word) {
     let result = '';
+    let found = false;
     for (const char of word) {
         for (const emoji of emojis) {
-            if (emoji.letter === char) {
+            if(emoji.letter === char.toLowerCase()) {
                 result += emoji.symbol;
+                found = true;
             }
         }
+        if(found == false) {
+            result += char;
+        }    
     }
+    return result;
 }
+
+module.exports = encodeWord;
--- main.js
+++ main.js
@@ -1,9 +1,26 @@
 const translateWord = require('./translate-word.js');
 const encodeWord = require('./encode-word.js');
 
+const command = process.argv[2];
+const words = process.argv.slice(3);
 
-const words = process.argv.slice(2);
-// const encoding = words.map(encodeWord);
-// console.log(encoding.join(' '));
-const translation = words.map(translateWord)
-console.log(translation.join(' '));
+const translation = words.map(translateWord);
+const encoding = words.map(encodeWord);
+
+switch(command) {
+    case 'translate':
+        console.log(translation.join(' '));
+        break;
+    case 'encode':
+        console.log(encoding.join(' '));
+        break;
+    default:
+        console.log('You have to type either translate or encode after main.js to change your sentence.');    
+        break; // you don't need it logically, but it's a good habit.
+    }
+
+
+
+
+
+
